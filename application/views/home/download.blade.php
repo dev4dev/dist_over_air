e55c5040e7f9e05c04b1986f203535e80df567d8
Alex Antonyuk <alex@antonyuk.me>
2013-01-10 23:23:17 +0200

    show empty message if no build was uploaded before

--- application/views/home/download.blade.php
+++ application/views/home/download.blade.php
@@ -5,5 +5,7 @@
 	@if ($app->info_version != '')
 		<div><a href="itms-services://?action=download-manifest&amp;url={{ $app->manifest_url() }}">Tap Here to Install {{ $app->info_version != '' ? 'v.' . $app->info_version : '' }}</a></div>
 		<div><a href="{{ $app->ipa_url() }}">Tap Here to Download {{ $app->info_version > 0 ? 'v.' . $app->info_version : '' }}</div>
+	@else
+		<div>Build something before...</div>
 	@endif
 @endsection
